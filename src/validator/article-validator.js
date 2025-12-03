cdfa81c7f079ef907c23a343cad20aec23cc115e
Leon <gomdole32@naver.com>
2025-12-03 16:41:19 +0900

    js 삭제

--- src/validator/article-validator.js
+++
@@ -1,9 +0,0 @@
-// TODO) Article-Validator: 유효성 검사
-import * as s from 'superstruct';
-
-export const CreateArticle = s.object({
-  title: s.size(s.string(), 1, 30),
-  content: s.size(s.string(), 1, 300),
-});
-
-export const PatchArticle = s.partial(CreateArticle);
--- src/validator/purchase-validator.js
+++
@@ -1,7 +0,0 @@
-// TODO) Purchase-Validator: 유효성 검사
-import * as s from 'superstruct';
-
-export const PurchaseProduct = s.object({
-  productId: s.min(s.integer(), 1),
-  quantity: s.min(s.integer(), 1),
-});
